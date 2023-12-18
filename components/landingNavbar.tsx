33967af2e58c6a87f9da256507b76c984b27f1d0
EmadDev1991 <emad.dev.1991@gmail.com>
2023-12-18 03:31:26 +0100

    fix extra height in landing page

--- components/landingNavbar.tsx
+++ components/landingNavbar.tsx
@@ -4,7 +4,7 @@ import { Button } from "./ui/button";
 
 const LandingNavbar = () => {
   return (
-    <nav className="w-full h-20 ">
+    <nav className="absolute w-full h-20 bg-[#111827]">
       <div className="max-w-screen-xl mx-auto px-2 md:px-6  h-full flex items-center justify-between">
         <Logo/>
         <Button variant='secondary'>Get Started</Button>
