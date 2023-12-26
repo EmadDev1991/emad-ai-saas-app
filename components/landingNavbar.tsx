dc4af6aae2b2ab408f0793e56cd6ab4a469de1d7
EmadDev1991 <emad.dev.1991@gmail.com>
2023-12-26 08:12:24 +0100

    fix bug in landing page dashboard button

--- components/landingNavbar.tsx
+++ components/landingNavbar.tsx
@@ -1,17 +1,23 @@
+"use client";
+
 import React from "react";
 import Logo from "./logo";
 import { Button } from "./ui/button";
-import { auth } from "@clerk/nextjs";
+import { useAuth } from "@clerk/nextjs";
 import Link from "next/link";
 
 const LandingNavbar = () => {
-  const user = auth()
+  const user = useAuth();
+
+  console.log(user);
   return (
     <nav className="absolute w-full h-20 bg-[#111827]">
       <div className="max-w-screen-xl mx-auto px-2 md:px-6  h-full flex items-center justify-between">
-        <Logo/>
-        <Link href={user ? '/dashboard': 'sign-in'}>
-          <Button variant='secondary'>{user? "Dashboard": "Get Started"}</Button>
+        <Logo />
+        <Link href={user ? "/dashboard" : "sign-in"}>
+          <Button variant="secondary">
+            {user.isSignedIn ? "Dashboard" : "Get Started"}
+          </Button>
         </Link>
       </div>
     </nav>
